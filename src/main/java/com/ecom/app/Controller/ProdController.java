6cc451ab27e10dce1b062b96018d9e91cfe062ef
Deakon89 <franck.devw@gmail.com>
2025-06-20 22:37:08 +0200

    fix

--- src/main/java/com/ecom/app/Controller/ProdController.java
+++ src/main/java/com/ecom/app/Controller/ProdController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(origins = "http://localhost:4200")
+
 public class ProdController {
 
     private final ProdService service;
